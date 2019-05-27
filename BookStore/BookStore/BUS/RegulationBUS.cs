ec168a0251a09dab33e525b1840b31756b790b9d
Qui <qui021098@gmail.com>
2019-05-28 05:39:50 +0700

    DAO BUS

---
+++ BookStore/BookStore/BUS/RegulationBUS.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BUS
+{
+    public class RegulationBUS
+    {
+        RegulationDAO DAO = new RegulationDAO();
+    }
+}
